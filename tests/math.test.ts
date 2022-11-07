08089bf5b0e4a1081fe036dcc4e5886b684eb40d
Dang Van Thanh <dangvanthanh@dangthanh.org>
2022-11-07 23:47:41 +0700

    Update math function

---
+++ tests/math.test.ts
@@ -0,0 +1,33 @@
+import { sum, subtract, multiply, divide } from './math';
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test('adds 2 + 3 to equal 5', () => {
+  expect(sum(2, 3)).toBe(5);
+});
+
+test('subtract 5 - 2 to equal 3', () => {
+  expect(subtract(5, 2)).toBe(3);
+});
+
+test('subtract 2 - 1 to equal 1', () => {
+  expect(subtract(2, 1)).toBe(1);
+});
+
+test('multiply 1 * 2 to equal 2', () => {
+  expect(multiply(1, 2)).toBe(2);
+});
+
+test('multiply 2 * 3 to equal 6', () => {
+  expect(multiply(2, 3)).toBe(6);
+});
+
+test('divide 4 / 2 to equal 2', () => {
+  expect(divide(4, 2)).toBe(2);
+});
+
+test('divide 6 / 0 to equal Infinity', () => {
+  expect(divide(6, 0)).toBe(Infinity);
+});
---
+++ tests/math.ts
@@ -0,0 +1,17 @@
+function sum(a: number, b: number): number {
+  return a + b;
+}
+
+function subtract(a: number, b: number): number {
+  return a - b;
+}
+
+function multiply(a: number, b: number): number {
+  return a * b;
+}
+
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+export { sum, subtract, multiply, divide };
