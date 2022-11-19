62ab1a7bd84b603be07af11c20d88aad8c435a29
Dang Van Thanh <thanhdang@poetadigital.com>
2022-11-19 18:27:11 +0700

    Constraining value types more test

--- tests/06-constraining-value-types/constraining-value-types.test.ts
+++ tests/06-constraining-value-types/constraining-value-types.test.ts
@@ -8,8 +8,23 @@ const defaultUser: User = {
 const defaultAdmin: User = {
 	name: 'Thanh',
 	role: 'admin',
-};
+}
+
+const defaultSuperAdmin: User = {
+	name: 'Thanh',
+	role: 'super admin',
+};;
+
+test('should get user is the user', () => {
+	expect(getUser(defaultUser)).toBe('Thanh is user');
+});
+
 
 test('should get user is the admin', () => {
-	expect(getUser(defaultUser)).toBe('Thanh is admin');
+	expect(getUser(defaultAdmin)).toBe('Thanh is admin');
+});
+
+
+test('should get user is the super admin', () => {
+	expect(getUser(defaultSuperAdmin)).toBe('Thanh is super admin');
 });
