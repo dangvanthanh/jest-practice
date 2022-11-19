13587634f83549d11f5d1c007de89f391fb11b59
Dang Van Thanh <thanhdang@poetadigital.com>
2022-11-19 22:10:12 +0700

    Test arrays

--- tests/07-arrays/arrays.test.ts
+++ tests/07-arrays/arrays.test.ts
@@ -1,15 +1,15 @@
-import {User, countPosts} from './arrays'
+import { User, countPosts } from './arrays';
 
 const defaultUser: User = {
-  id: 1,
-  name: 'Thanh Dang',
-  role: 'user',
-  posts: [
-    {id: 1, title: 'awsd'},
-    {id: 2, title: 'qwerty'}
-  ]
-}
+	id: 1,
+	name: 'Thanh Dang',
+	role: 'user',
+	posts: [
+		{ id: 1, title: 'awsd' },
+		{ id: 2, title: 'qwerty' },
+	],
+};
 
 test('count posts is 2', () => {
-  expect(countPosts(defaultUser)).toBe(2)
-})
+	expect(countPosts(defaultUser)).toBe(2);
+});
