e730372b6031eaf548b1bf9697cf16d8158b9720
Dang Van Thanh <dangvanthanh@dangthanh.org>
2022-12-04 22:19:56 +0700

    Update anotations.test.ts

---
+++ tests/08-annotations/anotations.test.ts
@@ -0,0 +1,13 @@
+import { User } from './anotations';
+
+test('Should return a valid user', () => {
+  const user = makeUser();
+
+  expect(user.id).toBeTypeOf("number");
+  expect(user.firstName).toBeTypeOf("string");
+  expect(user.lastName).toBeTypeOf("string");
+  expect(user.role).to.be.oneOf(["super-admin", "admin", "user"]);
+
+  expect(user.posts[0].id).toBeTypeOf("number");
+  expect(user.posts[0].title).toBeTypeOf("string");
+});
