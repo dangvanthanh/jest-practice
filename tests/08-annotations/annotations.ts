646ec524a62d1ab91f50172bbbf9b73597b4cee4
Dang Van Thanh <dangvanthanh@dangthanh.org>
2022-12-04 22:18:54 +0700

    Update annotations.ts

---
+++ tests/08-annotations/annotations.ts
@@ -0,0 +1,28 @@
+interface Post {
+  id: number;
+  title: string;
+}
+
+
+interface User {
+  id: number;
+  firstName: string;
+  lastName: string;
+  role: 'admin' | 'user' | 'super-admin';
+  posts: Array<Post>;
+}
+
+export const makeUser = (): User => {
+  return {
+    id: 1,
+    firstName: "Thanh",
+    lastName: "Dang",
+    role: "admin",
+    posts: [
+      {
+        id: 1,
+        title: "How I eat so much cheese",
+      },
+    ],
+  };
+};
